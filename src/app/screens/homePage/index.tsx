6a1c578bf630affe63740791f5a6e5f8488c2b64
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-08-03 14:25:19 +0900

    feat: develop ProductService  business logic to retrieve data from backend

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,53 +9,37 @@ import { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 //REDUX SLICE  & SELECTOR 
 
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data:Product[]) => dispatch(setPopularDishes(data))
   });
 
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-   (popularDishes) => ({popularDishes})
-  );
+
 
 export default function HomePage() {
 const {setPopularDishes} = actionDispatch(useDispatch());
-const {popularDishes} = useSelector(popularDishesRetriever);
 
   useEffect(() =>{  //Backend server data request -> Data
-    const result = [
-      {
-          "_id": "66a5124a5230cb3458e934e0",
-          "productStatus": "PROCESS",
-          "productCollection": "DRINK",
-          "productName": "Dena",
-          "productPrice": 12,
-          "productLeftCount": 100,
-          "productSize": "NORMAL",
-          "productVolume": 1,
-          "productImages": [
-              "uploads/products/cbd7ea18-7f3c-47c3-a735-a1558f93266b.png"
-          ],
-          "productView": 0,
-          "createdAt": "2024-07-27T15:29:14.137Z",
-          "updatedAt": "2024-07-27T15:29:14.137Z",
-          "__v": 0
-      }
-    ];
+   
+    const product = new ProductService();
 
-    // Slice: Data -> Store
-    //@ts-ignore
-    setPopularDishes(result);
+    product.getProducts(
+      {page:1,
+      limit:4,
+      order:"productViews",
+      productCollection: ProductCollection.DISH}
+    ).then((data) => {
+      setPopularDishes(data);
+    }).catch((err)=>{
+      console.log(err);
+    });
     
   },[]);
-
-  console.log('result  ---->', process.env.REACT_APP_API_URL);
 
     return <div className={"homepage"}>
       <Statistics/>
