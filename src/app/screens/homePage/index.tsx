5deb04b6ed79fb71f64c44fbb88e9509b98e7f80
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-08-03 14:55:11 +0900

    fix: retrieve fresh menu

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -4,37 +4,49 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisiment";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
 import { useEffect } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
-//REDUX SLICE  & SELECTOR 
+import "../../../css/home.css";
 
+//REDUX SLICE   (writing data to redux)
 const actionDispatch = (dispatch: Dispatch) => ({
-  setPopularDishes: (data:Product[]) => dispatch(setPopularDishes(data))
-  });
+        setPopularDishes: (data:Product[]) => dispatch(setPopularDishes(data)),
+        setNewDishes:     (data:Product[]) => dispatch(setNewDishes(data))
+});
 
 
 
 export default function HomePage() {
-const {setPopularDishes} = actionDispatch(useDispatch());
+const {setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+
 
   useEffect(() =>{  //Backend server data request -> Data
    
     const product = new ProductService();
 
-    product.getProducts(
-      {page:1,
+    product.getProducts({
+      page:1,
       limit:4,
       order:"productViews",
       productCollection: ProductCollection.DISH}
     ).then((data) => {
       setPopularDishes(data);
+    }).catch((err)=>{
+      console.log(err);
+    });
+
+    product.getProducts(
+      {page:1,
+      limit:4,
+      order:"createdAt",
+      //productCollection: ProductCollection.DISH
+    }).then((data) => {
+      setNewDishes(data);
     }).catch((err)=>{
       console.log(err);
     });
